918ba62db1f8d195fec688f78619f096d3a5ba0d
ireina7 <comcx.yang@gmail.com>
2021-02-05 17:12:24 +0800

    done

--- src/core.js
+++ src/core.js
@@ -1,9 +1,9 @@
 /* Core configuration js file */
 
 function debug() {
-  console.log("debug!")
+  console.log("debug!");
   //read_base()
-  set_file()
+  set_file();
 }
 
 
@@ -21,6 +21,8 @@ function Query_Unit(id, category, query, choice, answer) {
 
 var queries = [];
 var current_query_id = 0;
+var query_history = [];
+var goto_next_query = next_query;
 
 /**
  * Check for the various File API support.
@@ -47,14 +49,10 @@ function set_file() {
 
       // By lines
       var lines = this.result.split('\n');
-      queries = lines.map((line) => parse_line_from_base(line));
+      queries = lines.filter(line => line.trim() !== '').map((line) => parse_line_from_base(line));
+      console.log('Loaded ' + queries.length + ' queries.');
       next_query();
       //console.log(queries);
-      /*
-      for(var line = 0; line < lines.length; line++) {
-        console.log(lines[line]);
-      }
-      */
     };
     reader.readAsText(file);
   };
@@ -76,11 +74,15 @@ function next_query() {
     console.log("No query! Please load base file first of check your base file if empty!");
     return;
   }
-  if(current_query_id >= queries.length) {
+  if(current_query_id + 1 >= queries.length) {
     current_query_id = 0;
   } else {
     current_query_id += 1;
   }
+  if(queries[current_query_id] === undefined) {
+    current_query_id = 0;
+  }
+  document.getElementById('interaction').innerHTML = "";
   show_query(queries[current_query_id]);
 }
 function prev_query() {
@@ -93,15 +95,92 @@ function prev_query() {
   } else {
     current_query_id -= 1;
   }
+  if(queries[current_query_id] === undefined) {
+    current_query_id = 0;
+  }
+  document.getElementById('interaction').innerHTML = "";
+  show_query(queries[current_query_id]);
+}
+
+function get_random_Int(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+function rand_query() {
+  if(queries.length <= 0) {
+    console.log("No query! Please load base file first of check your base file if empty!");
+    return;
+  }
+  let rand_id = get_random_Int(queries.length);
+  current_query_id = rand_id;
+  if(queries[current_query_id] === undefined) {
+    current_query_id = 0;
+  }
+  document.getElementById('interaction').innerHTML = "";
   show_query(queries[current_query_id]);
 }
 
 function check_answer() {
-  console.log("checking....");
-  document.getElementById('interaction').innerHTML = "O";
-  //$('#interaction').innerHTML = "O";
-  return true;
+  let unit = queries[current_query_id];
+  let cat = unit.category;
+  var res = false;
+  if(cat == '单选题') {
+    res = check_answer_selection(unit);
+
+  } else if(cat == "多选题") {
+    res = check_answer_selection(unit);
+
+  } else if(cat == '判断题') {
+    res = check_answer_yes_no(unit);
+
+  } else if(cat == '填空题') {
+    res = check_answer_fill(unit);
+
+  } else {
+    console.log('Query category of ' + cat + ' is not supported!');
+    alert('Query category error, please reload base file.');
+    return false;
+  }
+  if(res === unit.answer) {
+    show_message('O');
+  } else {
+    show_message('X');
+  }
+  return res;
 }
+function show_answer() {
+  show_message(queries[current_query_id].answer);
+}
+
+function show_message(msg) {
+  document.getElementById('interaction').innerHTML = msg;
+}
+
+function check_answer_selection(unit) {
+  let a = $("#id0").is(":checked");
+  let b = $("#id1").is(":checked");
+  let c = $("#id2").is(":checked");
+  let d = $("#id3").is(":checked");
+  var ans = '';
+  if(a) ans += 'A';
+  if(b) ans += 'B';
+  if(c) ans += 'C';
+  if(d) ans += 'D';
+
+  return ans;
+}
+function check_answer_yes_no(unit) {
+  let yes = $("#id0").is(":checked");
+  let noo = $("#id1").is(":checked");
+  var ans = '';
+  if(yes) ans += 'Y';
+  if(noo) ans += 'N';
+
+  return ans;
+}
+function check_answer_fill(unit) {
+  document.getElementById('interaction').innerHTML = "O";
+}
+
 
 
 function show_query(unit) {
@@ -131,7 +210,10 @@ function create_selections(selections) {
     $("fieldset").append('<input type="checkbox" name="' + selections[i] + '" id="id' + i + '"><label for="id' + i + '">' + selections[i] + '</label>');
   }
   //$("#selection_board").append('<a href="#" data-role="button" data-inline="true" id="btndelcat">Elimina</a>');
-  $("#selection_board").append('<input type="button" value="Submit" onclick="check_answer()">');
+  $("#selection_board").append('<div data-role="navbar"><ul>\
+<li><a href="#" class="ui-btn-active" onclick="check_answer()">Submit</a>\</li>\
+<li><a href="#" onclick="show_answer()">Show answer</a>\
+</li></ul></div>');
   $("#selection_board").trigger('create');
 }
 
