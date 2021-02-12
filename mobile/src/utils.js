e3c713180c0dd6fdd2ecd992634c808465ee4cc3
ireina7 <comcx.yang@gmail.com>
2021-02-12 15:00:05 +0800

    resort

--- mobile/src/utils.js
+++
@@ -1,9 +0,0 @@
-export function dynamically_load_script(url) {
-    var script = document.createElement("script");
-    script.src = url;
-    document.head.appendChild(script);
-}
-export function get_random_Int(max) {
-    return Math.floor(Math.random() * Math.floor(max));
-}
-//# sourceMappingURL=utils.js.map
